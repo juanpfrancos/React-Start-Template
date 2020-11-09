05a4edc26b5f9ae757387b2d7b7ef89639d1d360
juanpfrancos <juanpanime@gmail.com>
2020-11-09 12:41:16 -0500

    First Commit.

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./styles/[name].css",
+    }),
+    new HtmlWebpackPlugin({
+      title: "React Base Template",
+      template: path.resolve(__dirname, "./src/index.html"),
+    }),
+  ],
+};
